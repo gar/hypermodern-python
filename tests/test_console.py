0b5e9a9a4b91c2e890730d4f370d584090f31f71
Gar Morley <garmorley@gmail.com>
2020-05-29 17:25:41 +0100

    Add mocks

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,5 +1,6 @@
 import click.testing
 import pytest
+import requests
 
 from hypermodern_python import console
 
@@ -9,11 +10,51 @@ def runner():
     return click.testing.CliRunner()
 
 
-def test_main_succeeds(runner):
+@pytest.fixture
+def mock_requests_get(mocker):
+    mock = mocker.patch("requests.get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorem Ipsum",
+        "extract": "Lorem ipsum dolor sit amet",
+    }
+    return mock
+
+
+@pytest.fixture
+def en_locale_mock(mocker):
+    mock = mocker.patch("locale.getlocale")
+    mock.return_value = ("en_US",)
+    return mock
+
+
+@pytest.fixture
+def mock_requests_get_fail(mocker):
+    mock = mocker.patch("requests.get")
+    mock.side_effect = requests.exceptions.HTTPError("404 Not Found")
+
+
+def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
 
 
-def test_main_failes_when_bad_lang_given(runner):
-    result = runner.invoke(console.main, ["--lang", "foobar"])
+def test_title_is_in_result(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert "Lorem Ipsum" in result.output
+
+
+def test_main_uses_locale_by_default(runner, mock_requests_get, en_locale_mock):
+    runner.invoke(console.main)
+    args, _ = mock_requests_get.call_args
+    assert "en.wikipedia.org" in args[0]
+
+
+def test_lang_can_be_overridden(runner, mock_requests_get, en_locale_mock):
+    runner.invoke(console.main, ["--lang", "es"])
+    args, _ = mock_requests_get.call_args
+    assert "es.wikipedia.org" in args[0]
+
+
+def test_main_fails_when_http_error(runner, mock_requests_get_fail):
+    result = runner.invoke(console.main)
     assert result.exit_code == 1
